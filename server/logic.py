bd55e4f3d0ee8ce929eff28e5f5b766aaeb0be7e
Pranav Pratheek Malleboyina <pranav.msadmissions@gmail.com>
2024-01-22 20:16:02 -0600

    completed the web backend part

---
+++ server/logic.py
@@ -0,0 +1,59 @@
+import json
+import pickle
+import numpy as np
+
+__locations = None
+__data_columns = None
+__model = None
+
+def get_estimated_price(location,sqft,bhk,bath):
+
+    global __model
+    with open(r"server\artifacts\RealEstateProject_model.pickle","rb") as f:
+        __model = pickle.load(f)
+        
+    try:
+        loc_index = __data_columns.index(location.lower())
+    except:
+        loc_index = -1
+
+    x = np.zeros(len(__data_columns))    
+
+    x[0] = sqft
+    x[1] = bhk
+    x[2] = bath
+
+    if loc_index >= 0 :
+        x[loc_index] = 1
+
+    return round(__model.predict([x])[0],2)
+
+def get_location_names():
+    global __locations
+    global __data_columns
+
+    with open(r"server\artifacts\real_estate_columns.json","r") as f:
+        __data_columns = json.load(f)['data columns']
+        __locations = __data_columns[3:]
+
+    return __locations
+
+def load_artifact_names():
+    global __data_columns
+
+    with open(r"server\artifacts\real_estate_columns.json","r") as f:
+        __data_columns = json.load(f)['data columns']
+        __locations = __data_columns[3:]
+
+    global __model
+    with open(r"server\artifacts\RealEstateProject_model.pickle","rb") as f:
+        __model = pickle.load(f)
+    print("loaded all the artifacts..")
+
+if __name__ == "__main__":
+    load_artifact_names()
+    print(__locations)
+    print(get_estimated_price("1st Phase JP Nagar",1000,2,2))
+    print(get_estimated_price("1st Phase JP Nagar",1000,3,3))
+    print(get_estimated_price("Kalhalli",1000,2,2))
+    print(get_estimated_price("Ejipura",1000,2,2))
--- server/server.py
+++ server/server.py
@@ -1,12 +1,24 @@
 from flask import Flask,request,jsonify
 app = Flask(__name__)
+import logic
 
 @app.route('/get_location_names')
 def get_location_names():
+    locations = logic.get_location_names()
+    response = jsonify({"locations" : locations})
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
+@app.route('/predict_home_price',methods=['POST'])
+def predict_home_price():
+    location = request.form['location']
+    sqft =  float(request.form['sqft'])
+    bhk =  int(request.form['bhk'])
+    bath = int(request.form['bath'])
+
     response = jsonify({
-        'locations':util.get_location_names()
+        "estimated price ": logic.get_estimated_price(location,sqft,bhk,bath)
     })
-    response.headers.add("Access-Control-Allow-Origin","*")
     return response
 
 if __name__ == "__main__":
